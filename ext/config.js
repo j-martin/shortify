095101f9e2da1ae797f0b994c1bfa742fb0351f9
Jean-Martin Archer <jm@jmartin.ca>
2019-02-23 20:03:38 -0800

    Remove dependency in jquery

--- ext/config.js
+++ ext/config.js
@@ -1,36 +1,31 @@
 // Saves options to localStorage.
 const saveOptions = () => {
-  localStorage['workspace'] = $('#workspace').val();
-  localStorage['urlPatterns'] = $('#urlPatterns').val();
-  localStorage['ignoredElements']=$('#ignoredElements').val();
-  localStorage['newWindow']=$('#newWindow').is(':checked');
+  localStorage['workspace'] = document.querySelector('#workspace').value;
+  localStorage['urlPatterns'] = document.querySelector('#urlPatterns').value;
+  localStorage['ignoredElements'] = document.querySelector('#ignoredElements').value;
+  localStorage['newWindow'] = document.querySelector('#newWindow').checked;
 };
 
 // Restores select box state to saved value from localStorage.
 const restoreOptions = () => {
-  let workspace = $('#workspace');
-  let urlPatterns = $('#urlPatterns');
-  let ignoredElements = $('#ignoredElements');
-  let newWindow = $('#newWindow');
+  let workspace = document.querySelector('#workspace');
+  let urlPatterns = document.querySelector('#urlPatterns');
+  let ignoredElements = document.querySelector('#ignoredElements');
+  let newWindow = document.querySelector('#newWindow');
 
-  setRealOnChange(workspace, saveOptions);
-  setRealOnChange(urlPatterns, saveOptions);
-  setRealOnChange(ignoredElements, saveOptions);
-  setRealOnChange(newWindow, saveOptions);
+  workspace.onchange = saveOptions;
+  urlPatterns.onchange = saveOptions;
+  ignoredElements.onchange = saveOptions;
+  newWindow.onchange = saveOptions;
 
-  workspace.val(localStorage['workspace']);
-  urlPatterns.val(localStorage['urlPatterns']);
-  ignoredElements.val(localStorage['ignoredElements']);
+  workspace.value = localStorage['workspace'];
+  urlPatterns.value = localStorage['urlPatterns'];
+  ignoredElements.value = localStorage['ignoredElements'];
 
   if (localStorage['newWindow'] === 'true') {
-    newWindow.attr('checked', 'checked');
+    newWindow.checked = true;
   }
 };
-
-const setRealOnChange = (field, onChangeMethod) => (() =>
-    field.bind('change keypress paste focus textInput input', () => {
-      onChangeMethod();
-    }))();
 
 document.addEventListener('DOMContentLoaded', () => {
   restoreOptions();
